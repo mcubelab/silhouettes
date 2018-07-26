f0219aa7691dbdc39369b6c5fc3c465e338246f7
mariabauza <mariabv525@gmail.com>
2018-07-25 21:55:48 -0400

    Maria needs to change branch

--- gathering/dense_automatic_bar_touching.py
+++ gathering/dense_automatic_bar_touching.py
@@ -2,29 +2,30 @@ import numpy as np
 from control_robot import ControlRobot
 
 if __name__ == "__main__":
-    experiment_name = "/media/mcube/data/shapes_data/pos_calib/full_bar_f=20_d=5_v1"
-    do_touch_motion = True
+    # experiment_name = "/media/mcube/data/shapes_data/pos_calib/full_bar_f=20_d=5_v1"
+    experiment_name = "/media/mcube/data/shapes_data/pos_calib/inclination_test"
+    do_touch_motion = False
 
 
     cr = ControlRobot(gs_ids=[2], force_list=[20])
-    gripper_rotation = "0"  # "0" "+30" "-30"
+    gripper_rotation = "-30"  # "0" "+30" "-30"
 
     if gripper_rotation == "0":
-        front_quat = 0.0, 0.7111, 0.7031, -0.0001
-        side_quat = 0.0, -0.00872, -1., 0.
-        back_quat = 0.0, 0.6846, -0.72888, 0.
+        front_pos = 674.69, 644.55, 748.03, 0.0, 0.7111, 0.7031, -0.0001
+        side_pos = 664.24, 612.12, 752.63, 0.0, -0.00872, -1., 0.
+        back_pos = 694.93, 650.38, 752.55, 0.0, 0.6846, -0.72888, 0.
     elif gripper_rotation == "+30":  # TODO: Put the right numbers here
-        front_quat = 0.0, 0.7111, 0.7031, -0.0001
-        side_quat = 0.0, -0.00872, -1., 0.
-        back_quat = 0.0, 0.6846, -0.72888, 0.
+        front_pos = 786., 650., 742., 0.08, -0.7111, -0.6855, 0.1
+        side_pos = 664.24, 550., 738., 0.018, 0.035, 0.98841, 0.146
+        back_pos = 793, 680.38, 752.55, 0.097, 0.6846, -0.72888, 0.
     elif gripper_rotation == "-30":  # TODO: Put the right numbers here
-        front_quat = 0.0, 0.7111, 0.7031, -0.0001
-        side_quat = 0.0, -0.00872, -1., 0.
-        back_quat = 0.0, 0.6846, -0.72888, 0.
+        front_pos = 588., 660., 740., 0.08, 0.7111, 0.6855, 0.06
+        side_pos = 635., 782., 727., 0.03, 0.006, 0.9826, -0.183
+        back_pos = 587., 651., 734., 0.097, -0.6846, 0.71, -0.1
 
     ###################### Front
     print 'RELOCATING...'
-    cart = 674.69, 644.55, 748.03 + front_quat
+    cart = front_pos
     cr.set_cart_mm(cart)
 
     print 'TOUCH MOTION'
@@ -54,7 +55,7 @@ if __name__ == "__main__":
     cart = 715.71, 619.77, 748.03,  0.0, -0.00872, -1., 0.
     cr.set_cart_mm(cart)
 
-    cart = 664.24, 612.12, 752.63 + side_quat
+    cart = side_pos
     cr.set_cart_mm(cart)
 
     print 'TOUCH MOTION'
@@ -84,7 +85,7 @@ if __name__ == "__main__":
     cart = 764.87, 670.99, 752.58,  0.0, 0.6846, -0.72888, 0.
     cr.set_cart_mm(cart)
 
-    cart = 694.93, 650.38, 752.55 + back_quat
+    cart = back_pos
     cr.set_cart_mm(cart)
 
     print 'TOUCH MOTION'
